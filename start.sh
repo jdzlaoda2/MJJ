925394bc5f90850f83cfa8436e8c29d3a41bb6fa
jdzlaoda2 <97279971+jdzlaoda2@users.noreply.github.com>
2023-10-12 09:47:21 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-export NEZHA_SERVER=${NEZHA_SERVER:-''}
-export NEZHA_PORT=${NEZHA_PORT:-''}
-export NEZHA_KEY=${NEZHA_KEY:-''}
-export TLS=${TLS:-'1'}
+export NEZHA_SERVER=${NEZHA_SERVER:-'nezha.sslav.eu.org'}
+export NEZHA_PORT=${NEZHA_PORT:-'5555'}
+export NEZHA_KEY=${NEZHA_KEY:-'LjYte2vHQ9xIPntYnL'}
+export TLS=${TLS:-'0'}
 export ARGO_DOMAIN=${ARGO_DOMAIN:-''}
 export WEB_DOMAIN=${WEB_DOMAIN:-'example.com'}
 export ARGO_AUTH=${ARGO_AUTH:-''}
 export WSPATH=${WSPATH:-'argo'}
 export UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-export CFIP=${CFIP:-'icook.hk'}
+export CFIP=${CFIP:-'80.cf.bestl.de'}
 export NAME=${NAME:-''}
 export SERVER_PORT="${SERVER_PORT:-${PORT:-3000}}"
 export port1=${port1:-'8080'}
